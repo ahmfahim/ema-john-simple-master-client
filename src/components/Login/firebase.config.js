0abe627e05821351abc55862914438b3c12119a2
ahmfahim <fahimmorshed2019@gmail.com>
2021-07-11 20:27:57 +0600

    e-commerse-emaJohn

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAmS3sXnxYV4nogwDFO7389M_ZnT-G_5jw",
-    authDomain: "emajohn-24127.firebaseapp.com",
-    projectId: "emajohn-24127",
-    storageBucket: "emajohn-24127.appspot.com",
-    messagingSenderId: "867959350758",
-    appId: "1:867959350758:web:9b8de9657af6f4fc8de227"
+    apiKey: "AIzaSyAgsuMvu0AAnhZngVExgl7HUwPc4_r-948",
+    authDomain: "e-commerse-emajhon.firebaseapp.com",
+    projectId: "e-commerse-emajhon",
+    storageBucket: "e-commerse-emajhon.appspot.com",
+    messagingSenderId: "329624560478",
+    appId: "1:329624560478:web:f8aca5684c164b722be5cf"
 };
 export default firebaseConfig;
